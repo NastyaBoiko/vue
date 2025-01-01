4ecf57873db2971b3feac21f94609a23df3cd340
Nastya <killer99whale@mail.ru>
2025-01-02 00:12:43 +0300

    basics

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 export default {
   data() {
     return {
-      ticker: 'default',
+      ticker: 'BTC',
       tickers: [
         {
           name: 'DEMO1',
@@ -17,6 +17,7 @@ export default {
           price: '1203',
         },
       ],
+      sel: null,
     }
   },
   methods: {
@@ -26,6 +27,13 @@ export default {
         price: '120NEW',
       }
       this.tickers.push(newTicker)
+      setInterval(async () => {
+        const f = await fetch(
+          // `https://min-api.cryptocompare.com/data/price?fsym=${newTicker.name}&tsyms=USD&api_key=420e35e35de3537d0d11caac933112798702e36d5826ff16798828fb34192e5a`,
+        )
+        const data = await f.json()
+        console.log(data)
+      }, 3000)
       this.ticker = ''
     },
     handleDelete(t) {
@@ -110,6 +118,10 @@ export default {
               <div
                 v-for="t in tickers"
                 :key="t.name"
+                @click="sel = t"
+                :class="{
+                  'border-2': sel === t,
+                }"
                 class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
               >
                 <div class="px-4 py-5 sm:p-6 text-center">
@@ -118,7 +130,7 @@ export default {
                 </div>
                 <div class="w-full border-t border-gray-200"></div>
                 <button
-                  @click="handleDelete(t)"
+                  @click.stop="handleDelete(t)"
                   class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
                 >
                   <svg
@@ -140,15 +152,15 @@ export default {
             <hr class="w-full border-t border-gray-600 my-4" />
           </template>
 
-          <section class="relative">
-            <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">VUE - USD</h3>
+          <section v-if="sel" class="relative">
+            <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">{{ sel.name }} - USD</h3>
             <div class="flex items-end border-gray-600 border-b border-l h-64">
               <div class="bg-purple-800 border w-10 h-24"></div>
               <div class="bg-purple-800 border w-10 h-32"></div>
               <div class="bg-purple-800 border w-10 h-48"></div>
               <div class="bg-purple-800 border w-10 h-16"></div>
             </div>
-            <button type="button" class="absolute top-0 right-0">
+            <button @click="sel = null" type="button" class="absolute top-0 right-0">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 xmlns:xlink="http://www.w3.org/1999/xlink"
