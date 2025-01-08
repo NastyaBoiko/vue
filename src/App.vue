62bf72952a6a534f69df6a20592a17fbf1424235
Nastya <a.boiko17@mail.ru>
2025-01-08 21:50:33 +0300

    animate-spin when notMounted

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ export default {
       sel: null,
       graph: [],
       apiTickers: [],
+      notMounted: true,
     }
   },
   async mounted() {
@@ -14,6 +15,7 @@ export default {
     for (let ticker in tickersObj.Data) {
       this.apiTickers.push(ticker)
     }
+    this.notMounted = false
   },
   methods: {
     add() {
@@ -87,6 +89,32 @@ export default {
     </head>
     <body>
       <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+        <div
+          v-if="notMounted"
+          class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center"
+        >
+          <svg
+            class="animate-spin -ml-1 mr-3 h-12 w-12 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
+
         <div class="container">
           <section>
             <div class="flex">
